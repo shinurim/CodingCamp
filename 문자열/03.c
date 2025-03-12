6a6370764659033ea1fc1c285dbe6ae4120c453e
yurim <120655247+shinurim@users.noreply.github.com>
2025-03-12 22:55:43 +0900

    Update and rename 3.c to 03.c

--- 문자열/03.c
+++ 문자열/03.c
@@ -4,7 +4,7 @@ int main(void) {
 	int k = 0;
 	char s[20];
 	char s1[20];
-	printf("���ڿ��� �Է��Ͻÿ� : ");
+	printf("¹®ÀÚ¿­À» ÀÔ·ÂÇÏ½Ã¿À : ");
 	gets(s);
 	for (int i = 0; s[i] != '\0'; i++) {
 		if (s[i] != ' ') {
