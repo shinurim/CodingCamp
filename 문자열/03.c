5cd1af15816f71bf2c17a12e7eb7a1afa389dbb2
yurim <120655247+shinurim@users.noreply.github.com>
2025-03-12 22:59:59 +0900

    Update 03.c

--- 문자열/03.c
+++ 문자열/03.c
@@ -4,7 +4,7 @@ int main(void) {
 	int k = 0;
 	char s[20];
 	char s1[20];
-	printf("¹®ÀÚ¿­À» ÀÔ·ÂÇÏ½Ã¿À : ");
+	printf("문자열을 입력하시오 : ");
 	gets(s);
 	for (int i = 0; s[i] != '\0'; i++) {
 		if (s[i] != ' ') {
